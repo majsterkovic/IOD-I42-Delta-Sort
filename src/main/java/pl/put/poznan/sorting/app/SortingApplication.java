a5c89bf81e33d3ff0da81d3bfbadb837e7a28fd2
majsterkovic <mariusz8h@o2.pl>
2022-12-04 22:37:26 +0100

    Utworzenie ramy architektonicznej dla SortingMadness
    Pakiety: app, logic, rest

---
+++ src/main/java/pl/put/poznan/sorting/app/SortingApplication.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.sorting.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.sorting.rest"})
+public class SortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
@@ -0,0 +1,44 @@
+package pl.put.poznan.sorting.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.sorting.logic.SortingMadness;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/{method}/{order}")
+public class SortingMadnessController {
+
+        private static final Logger logger = LoggerFactory.getLogger(SortingMadnessController.class);
+
+        @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+        public String [] get(@PathVariable String method, @PathVariable String order, @RequestParam(value="numbers") String[] numbers) {
+
+                // log the parameters
+                logger.debug(Arrays.toString(numbers));
+
+                // perform the transformation, you should run your logic here, below is just a silly example
+                SortingMadness sorting = new SortingMadness(numbers);
+                return sorting.sort();
+        }
+
+        @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+        public String [] post(@PathVariable String method, @PathVariable String order, @RequestBody String[] numbers) {
+
+                // log the parameters
+                logger.debug(Arrays.toString(numbers));
+
+                // perform the transformation, you should run your logic here, below is just a silly example
+                SortingMadness sorting = new SortingMadness(numbers);
+                return sorting.sort();
+        }
+
+
+
+}
+
+
