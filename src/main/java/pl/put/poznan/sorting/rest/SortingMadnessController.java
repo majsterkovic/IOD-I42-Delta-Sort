13d2f32c2755510d843ab2aa1e619f6052872aab
majsterkovic <mariusz8h@o2.pl>
2022-12-10 17:55:16 +0100

    Rozszerzenie metody get o wybór algorytmu sortowania i tworzenie odpowiedzi.

--- src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
@@ -3,12 +3,13 @@ package pl.put.poznan.sorting.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.sorting.logic.DummySort;
+import pl.put.poznan.sorting.logic.*;
 import pl.put.poznan.sorting.logic.SortContext;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -19,17 +20,17 @@ public class SortingMadnessController {
 
         /** sample request
 
-        GET localhost:8080/
-        Content-Type: application/json
-        Accept: application/json
+         GET localhost:8080/
+         Content-Type: application/json
+         Accept: application/json
 
          {
-                 "data": [1, 3, 2, 5, 4],
-                 "algorithms": "bubble, insertion, selection",
-                 "direction": "asc"
+         "data": [1, 3, 2, 5, 4],
+         "algorithms": "bubble, insertion, selection",
+         "direction": "asc"
          }
 
-        */
+         */
 
 
         @RequestMapping(method = RequestMethod.GET, consumes =  "application/json", produces = "application/json")
@@ -38,9 +39,13 @@ public class SortingMadnessController {
                 System.out.println("requestData: " + requestData.toString());
 
                 String[] algorithms;
+                String[] data;
                 String direction = "asc";
 
-                // TODO: get data from request, it may be strings or numbers or objects, get objects from JSON
+                String [] sortedData;
+                int sortingTime;
+
+                Map<String, Object> response = new HashMap<String, Object>();
 
                 algorithms = requestData.get("algorithms").toString().split(",");
 
@@ -48,8 +53,34 @@ public class SortingMadnessController {
                         direction = requestData.get("direction").toString();
                 }
 
-                // TODO: sort
-                return requestData;
+                // TODO: get data from request, it may be strings or numbers or objects, get objects from JSON
+                // How to handle it?
+
+                data = requestData.get("data").toString().replaceAll("\\[","").replaceAll("]","").replaceAll(" ", "").split(",");
+
+
+                SortContext sortContext;
+
+                // TODO: add direction parameter
+                // TODO: sort objects, not only strings and numbers
+
+                for (String algorithm : algorithms) {
+                        sortContext = switch (algorithm) {
+                                case "bubble" -> new SortContext(new BubbleSort());
+                                case "insertion" -> new SortContext(new InsertionSort());
+                                case "selection" -> new SortContext(new SelectionSort());
+                                case "heap" -> new SortContext(new HeapSort());
+                                default -> new SortContext(new DummySort());
+                                // case "merge" -> new SortContext(new MergeSort());
+                                // case "quick" -> new SortContext(new QuickSort());
+                        };
+                        sortedData = sortContext.sort(data);
+                        sortingTime = 0;
+
+                        response.put(algorithm, Arrays.asList(sortingTime, sortedData));
+                }
+
+                return response;
         }
 
         @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
@@ -58,8 +89,4 @@ public class SortingMadnessController {
                 // TODO: implement POST
         }
 
-
-
 }
-
-
