19d425ea54748d81cecb887bfd6f463895108254
AgnieszkaGap <115652886+AgnieszkaGap@users.noreply.github.com>
2022-12-20 00:55:44 +0100

    Komentarze do dokumentacji klasy SortStrategy

--- src/main/java/pl/put/poznan/sorting/logic/SortStrategy.java
+++ src/main/java/pl/put/poznan/sorting/logic/SortStrategy.java
@@ -1,10 +1,17 @@
 package pl.put.poznan.sorting.logic;
 
 /**
- * Sorting strategy.
+ * Sorting strategy implementation.
  */
 public interface SortStrategy {
 
+    /**
+     * Sorting method strategy.
+     * Method to define strategy for all sorting classes
+     *
+     * @param   input, type: Object[]
+     * @param   key, type: String
+     */
     public Object[] sort(Object[] input, String key);
 
 }
