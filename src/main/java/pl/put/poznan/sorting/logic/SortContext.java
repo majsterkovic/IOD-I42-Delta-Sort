cc2f90108e66aa2ca329241677ba3f7a6e07cc46
AgnieszkaGap <115652886+AgnieszkaGap@users.noreply.github.com>
2022-12-20 00:52:47 +0100

    Komentarze do dokumentacji klasy SortContext

--- src/main/java/pl/put/poznan/sorting/logic/SortContext.java
+++ src/main/java/pl/put/poznan/sorting/logic/SortContext.java
@@ -1,13 +1,30 @@
 package pl.put.poznan.sorting.logic;
 
+/**
+ * Sorting Context implementation.
+ *
+ */
+
 public class SortContext {
 
     private final SortStrategy strategy;
-
+    
+    /**
+     * Method to asaign strategy
+     *
+     * @param   strategy    type: SortStrategy
+     */
     public SortContext(SortStrategy strategy) {
         this.strategy = strategy;
     }
-
+    
+    /**
+     * Method to sort using a strategy
+     *
+     * @param   data
+     * @param   sortKey
+     * @return  sort with strategy
+     */
     public Object[] sort(Object[] data, String sortKey) {
         return strategy.sort(data, sortKey);
     }
