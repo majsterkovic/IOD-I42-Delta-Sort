4bf920b708e1ab6a4eac8df85cbc54b90be5cbd6
Szymon3125 <50416224+Szymon3125@users.noreply.github.com>
2022-12-20 17:13:34 +0100

    dodano zwracanie komunikatów w przypadku blednego zapytania

--- src/main/java/pl/put/poznan/sorting/rest/SimpleArraySortingController.java
+++ src/main/java/pl/put/poznan/sorting/rest/SimpleArraySortingController.java
@@ -12,7 +12,6 @@ import pl.put.poznan.sorting.app.SortingMadness;
 import pl.put.poznan.sorting.models.SortRequest;
 import pl.put.poznan.sorting.models.SortResult;
 
-import java.util.HashMap;
 import java.util.Map;
 /**
  * Request to sort object json implementation for simple arrays.
@@ -43,21 +42,25 @@ public class SimpleArraySortingController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
+        if (request.algorithms == null || request.algorithms.length == 0) {
+            logger.error("No algorithm provided.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No algorithm provided.");
+        }
+
         if (request.data == null || request.data.length == 0) {
             logger.error("No data provided.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No data provided.");
         }
 
         for (Object value : request.data) {
+            if (!(value instanceof String || value instanceof Float || value instanceof Double || value instanceof Integer || value instanceof Long)) {
+                logger.error("Type of value to compare is not supported.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Type of value to compare is not supported.");
+            }
             if (value.getClass() != request.data[0].getClass()) {
                 logger.error("Data have different types.");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data have different types.");
             }
-        }
-
-        if (request.algorithms == null || request.algorithms.length == 0) {
-            logger.error("No algorithm provided.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No algorithm provided.");
         }
 
         logger.debug("Initializing sorter.");
@@ -65,20 +68,45 @@ public class SimpleArraySortingController {
         SortingMadness madness = new SortingMadness(request.data, request.algorithms, request.reverse, request.iterations);
         SortResult[] result = madness.getResult();
 
+        logger.debug("Returning result.");
         return ResponseEntity.ok(result);
     }
 
     @GetMapping(value = "/{algorithms}/{reverse}/{iterations}", consumes = "application/json", produces = "application/json")
-    public Map<String, Object> get(@PathVariable("algorithms") String[] algorithms,
+    public ResponseEntity<Object> get(@PathVariable("algorithms") String[] algorithms,
                                    @PathVariable("reverse") boolean reverse,
                                    @PathVariable("iterations") int iterations,
                                    @RequestParam("data") Object[] data) {
 
         System.out.println("Received new request.");
+
+        if (algorithms == null || algorithms.length == 0) {
+            logger.error("No algorithm provided.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No algorithm provided.");
+        }
+
+        if (data == null || data.length == 0) {
+            logger.error("No data provided.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No data provided.");
+        }
+
+        for (Object value : data) {
+            if (!(value instanceof String || value instanceof Float || value instanceof Double || value instanceof Integer || value instanceof Long)) {
+                logger.error("Type of value to compare is not supported.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Type of value to compare is not supported.");
+            }
+            if (value.getClass() != data[0].getClass()) {
+                logger.error("Data have different types.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data have different types.");
+            }
+        }
+
+        logger.debug("Initializing sorter.");
         SortingMadness madness = new SortingMadness(data, algorithms, reverse, iterations);
-        Map<String, Object> result = new HashMap<>();
-        result.put("result", madness.getResult());
-        return result;
+        SortResult[] result = madness.getResult();
+
+        logger.debug("Returning result.");
+        return ResponseEntity.ok(result);
     }
 
 }
