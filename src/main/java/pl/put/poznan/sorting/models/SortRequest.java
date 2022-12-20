4044b59af3c071eea20e81f6cee4dcd12eb28ebe
AgnieszkaGap <115652886+AgnieszkaGap@users.noreply.github.com>
2022-12-20 01:37:58 +0100

    Komentarze do dokumentacji klasy SortRequest

--- src/main/java/pl/put/poznan/sorting/models/SortRequest.java
+++ src/main/java/pl/put/poznan/sorting/models/SortRequest.java
@@ -5,8 +5,21 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.ToNumberPolicy;
 
+/**
+ * Request sort - json implementation for simple arrays.
+ *
+ */
 public class SortRequest {
-
+    
+    /**
+     * Assigning arguments to class fields.
+     *
+     * @param   data
+     * @param   algorithms
+     * @param   key
+     * @param   reverse
+     * @param   iterations
+     */
     public SortRequest(Object[] data, String[] algorithms, boolean reverse, int iterations) {
         this.data = data;
         this.algorithms = algorithms;
@@ -19,10 +32,22 @@ public class SortRequest {
     public final boolean reverse;
     public final int iterations;
     
+    /**
+     * Creating json Object.
+     *
+     * @param   json    type: String
+     * @return          new json object
+     * @throws JsonSyntaxException
+     */
     public static SortRequest fromJson(String json) throws JsonSyntaxException {
         return new GsonBuilder().setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE).create().fromJson(json, SortRequest.class);
     }
 
+    /**
+     * Transorming to json.
+     *
+     * @return      object transformed to json format.
+     */
     public String toJson() {
         return new Gson().toJson(this);
     }
