fac15111b7c238c5b58ca4b43c5dc9e29e197b74
AgnieszkaGap <115652886+AgnieszkaGap@users.noreply.github.com>
2022-12-20 01:12:29 +0100

    Komentarze do dokumentacji klasy Timer

--- src/main/java/pl/put/poznan/sorting/logic/Timer.java
+++ src/main/java/pl/put/poznan/sorting/logic/Timer.java
@@ -1,21 +1,44 @@
 package pl.put.poznan.sorting.logic;
 
+/**
+ * Timing implementation.
+ *
+ */
 public class Timer {
     private long startTime;
     private long endTime;
 
+    /**
+     * Start of measuring time.
+     * Assigning the result to a class field startTime.
+     */
     public void startMeasure() {
         startTime = System.nanoTime();
     }
 
+    /**
+     * End of measuring time.
+     * Assigning the result to a class field endTime.
+     */
     public void stopMeasure() {
         endTime = System.nanoTime();
     }
 
+    /**
+     * Calculating the time from start point to end.
+     *
+     * @return  measured time
+     */
     public String getElapsedTime() {
         return String.valueOf((endTime - startTime) / 1000000.0);
     }
 
+    /**
+     * Calculating the time from start ponit to end in chosen format.
+     *
+     * @param   format  seconds, miliseconds, minutes, minutes with seconds
+     * @return          measured time in a chosen format
+     */
     public String getElapsedTime(String format) {
         switch (format) {
             case "ms" -> {
