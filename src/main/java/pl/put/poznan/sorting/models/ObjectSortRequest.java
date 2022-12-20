095b5fc9fc0437dd4ed5c4e2b49b4cb6436e55af
AgnieszkaGap <115652886+AgnieszkaGap@users.noreply.github.com>
2022-12-20 01:33:49 +0100

    Komentarze do dokumentacji klasy ObjectSortRequest

--- src/main/java/pl/put/poznan/sorting/models/ObjectSortRequest.java
+++ src/main/java/pl/put/poznan/sorting/models/ObjectSortRequest.java
@@ -3,8 +3,21 @@ package pl.put.poznan.sorting.models;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
+/**
+ * Request sort - json implementation for objects.
+ *
+ */
 public class ObjectSortRequest extends SortRequest {
 
+    /**
+     * Assigning arguments to class fields.
+     *
+     * @param   data
+     * @param   algorithms
+     * @param   key
+     * @param   reverse
+     * @param   iterations
+     */
     public ObjectSortRequest(Object[] data, String[] algorithms, String key, boolean reverse, int iterations) {
         super(data, algorithms, reverse, iterations);
         this.key = key;
@@ -12,10 +25,22 @@ public class ObjectSortRequest extends SortRequest {
 
     public final String key;
 
+    /**
+     * Creating json Object.
+     *
+     * @param   json    type: String
+     * @return          new json object
+     * @throws  JsonSyntaxException
+     */
     public static ObjectSortRequest fromJson(String json) throws JsonSyntaxException {
         return new Gson().fromJson(json, ObjectSortRequest.class);
     }
-
+    
+    /**
+     * Transorming to json.
+     * 
+     * @return      object transformed to json format.
+     */
     public String toJson() {
         return new Gson().toJson(this);
     }
